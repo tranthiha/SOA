f972f7dc5e9d78ab805c35b8a480c690b1eaecdf
nguyenquangminh <nevermore164@gmail.com>
2018-03-26 23:22:23 +0700

    update

--- soa_web/app/controllers/api/v1/student/book_borrows_controller.rb
+++ soa_web/app/controllers/api/v1/student/book_borrows_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::Student::BookBorrowController < ApplicationController
+class Api::V1::Student::BookBorrowsController < ApplicationController
   skip_before_action :verify_authenticity_token
   before_action -> {pre_processing}
 
